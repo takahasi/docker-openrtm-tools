f8719773f595291e2fc1003d605ecbaeff6b700d
takahasi <3263ta@gmail.com>
2017-12-03 22:39:48 +0900

    tools: update rtmdocker.py

--- rtmdocker.py
+++ rtmdocker.py
@@ -2,111 +2,109 @@ import sys
 import os
 from argparse import ArgumentParser
 
-prog = __file__
-version = "1.0.0"
 
-# TAG NAME (default)
-tag = "latest"
+class Rtmdocker:
+    def __init__(self):
+        self._platform = sys.platform
+        self._args = self.parser()
 
-# Docker Hub Image
-image = "takahasi/docker-openrtm:" + tag
+    def start(self):
+        command = self.assume_command(self._args.command)
+        self._command = self.assume_options(self._args, command)
+        if self._args.xforward:
+            self.enable_x()
 
-# Docker Option
-entry = os.getcwd()
-option = "-v $HOME:$HOME:rw --privileged=true -e ENTRY=" + entry
-option_network = "--net=host"
-option_display = "-e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/.Xauthority:/root/.Xauthority"
+        print(self._command)
+        os.system(self._command)
 
-pf = sys.platform
+        if self._args.xforward:
+            self.disable_x()
 
+        return
 
-def parser():
-    usage = 'Usage: python {} [-v] [-n] [-t <tag>] [-x] [-c <comp>] [-r <comp>] [--help] command'.format(__file__)
-    argparser = ArgumentParser(usage=usage)
-    argparser.add_argument('command', type=str, help='command')
-    argparser.add_argument('-v', '--version', action='store_true', help='print version')
-    argparser.add_argument('-n', '--nameserver', action='store_true', help='run command with starting nameserver')
-    argparser.add_argument('-c', '--compile', type=str, dest='c_component', help='compile your C++ component')
-    argparser.add_argument('-t', '--tag', type=str, dest='tagname', help='tag name of image')
-    argparser.add_argument('-r', '--run', type=str, dest='r_component', help='run your component')
-    argparser.add_argument('-x', '--xforward', action='store_true', help='enable X forwarding')
-    return argparser.parse_args()
+    def parser(self):
+        usage = 'Usage: python {} [-v] [-n] [-t <tag>] [-x] [-c <comp>] [-r <comp>] [--help] command'.format(__file__)
+        argparser = ArgumentParser(usage=usage)
+        argparser.add_argument('command', type=str, default='bash', help='command')
+        argparser.add_argument('-v', '--version', action='version', version='%(prog)s 1.0.0')
+        argparser.add_argument('-n', '--nameserver', action='store_true', help='run command with starting nameserver')
+        argparser.add_argument('-c', '--compile', type=str, dest='compile_component', help='compile your C++ component')
+        argparser.add_argument('-t', '--tag', type=str, dest='tagname', default='latest', help='tag name of image')
+        argparser.add_argument('-r', '--run', type=str, dest='run_component', help='run your component')
+        argparser.add_argument('-x', '--xforward', action='store_true', help='enable X forwarding')
+        return argparser.parse_args()
 
+    def assume_command(self, command):
+        example = "/usr/share/openrtm-1.1/example"
+        example_py = "python " + example + "/python"
 
-def set_command(s):
-    example = "/usr/share/openrtm-1.1/example"
-    example_py = "$example/python"
+        c_dict = {"openrtp": "openrtp",
+                  "Controller": example + "/ControllerComp",
+                  "Motor": example + "/MotorComp",
+                  "ConsoleIn": example + "/ConsoleInComp",
+                  "ConsoleOut": example + "/ConsoleOutComp",
+                  "SeqIn": example + "/SeqInComp",
+                  "SeqOut": example + "/SeqOutComp",
+                  "MyServiceConsumer": example + "/MyServiceConsumerComp",
+                  "MyServiceProvider": example + "/MyServiceProviderComp",
+                  "ConsoleInPy": example_py + "/SimpleIO/ConsoleIn.py",
+                  "ConsoleOutPy": example_py + "/SimpleIO/ConsoleOut.py",
+                  "SeqInPy": example_py + "/SeqIO/SeqIn.py",
+                  "SeqOutPy": example_py + "/SeqIO/SeqOut.py",
+                  "MyServiceConsumerPy": example_py + "/SimpleService/MyServiceConsumerComp",
+                  "MyServiceProviderPy": example_py + "/SimpleService/MyServiceProviderComp",
+                  "TkJoyStick": example_py + "/TkJoyStick/TkJoyStickComp.py",
+                  "TkLRFViewer": example_py + "/TkLRFViewer/TkLRFViewer.py",
+                  "bash": "bash",
+                  }
 
-    c_dict = {"openrtp": "openrtp",
-              "Controller": example + "/ControllerComp",
-              "Motor": example + "/MotorComp",
-              "ConsoleIn": example + "/ConsoleInComp",
-              "ConsoleOut": example + "/ConsoleOutComp",
-              "SeqIn": example + "/SeqInComp",
-              "SeqOut": example + "/SeqOutComp",
-              "MyServiceConsumer": example + "/MyServiceConsumerComp",
-              "MyServiceProvider": example + "/MyServiceProviderComp",
-              "ConsoleInPy": "python" + example_py + "/ConsoleIn.py",
-              "ConsoleOutPy": "python" + example_py + "/ConsoleOut.py",
-              "SeqInPy": "python " + example_py + "/SeqIn.py",
-              "SeqOutPy": "python " + example_py + "/SeqOut.py",
-              "MyServiceConsumerPy": "python " + example_py + "/MyServiceConsumerComp",
-              "MyServiceProviderPy": "python " + example_py + "/MyServiceProviderComp",
-              "TkJoyStick": "python " + example_py + "/TkJoyStickComp.py",
-              "TkLRFViewer": "python " + example_py + "/TkLRFViewer.py",
-              "bash": "bash",
-              }
+        c = c_dict[command]
+        if c is None:
+            return "bash"
+        else:
+            return c
 
-    c = c_dict[s]
-    if c is None:
-        return "bash"
-    else:
-        return c
-
-
-def enable_x():
-    os.system("xhost local: > /dev/null")
-    return
-
-
-def disable_x():
-    os.system("xhost - > /dev/null")
-    return
-
-
-def main():
-    args = parser()
-    if pf == "win32":
-        print("not yet")
-    else:
-        # darwin, linux
-        command = set_command(args.command)
+    def assume_options(self, args, command):
+        # Docker Option
+        entry = os.getcwd()
 
         option_list = []
+        option = "-v $HOME:$HOME:rw --privileged=true -e ENTRY=" + entry
         option_list.append(option)
 
         if args.xforward:
-            enable_x()
+            option_display = "-e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/.Xauthority:/root/.Xauthority"
             option_list.append(option_display)
 
         if args.nameserver:
             command = "rtm-naming;" + command
 
-        if args.compile:
+        if args.compile_component:
             command = "apt-get update && apt-get -y install cmake && cd " + entry + " && mkdir -p build && cd build cmake .. && make"
 
-        if args.run:
-            command = args.r_component
+        if args.run_component:
+            command = args.run_component
 
+        option_network = "--net=host"
         option_list.append(option_network)
-        option_list.append(image)
+        option_list.append("takahasi/docker-openrtm:" + args.tagname)
         option_list.append("\"" + command + "\"")
-        print("docker run -ti --rm " + " ".join(option_list))
-        os.system("docker run -ti --rm " + " ".join(option_list))
+        return "docker run -ti --rm " + " ".join(option_list)
 
-        if args.xforward:
-            disable_x()
+    def enable_x(self):
+        if self._platform != "win32":
+            os.system("xhost local: > /dev/null")
+        return
 
+    def disable_x(self):
+        if self._platform != "win32":
+            os.system("xhost - > /dev/null")
+        return
+
+
+def main():
+    rtmdocker = Rtmdocker()
+    rtmdocker.start()
     return
 
 if __name__ == "__main__":
