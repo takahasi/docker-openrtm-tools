b981e5fb344f43b3a614ece38cf96bcb0be09e60
takahasi <3263ta@gmail.com>
2017-12-12 16:15:38 +0900

    tools: add OpenCV component

--- rtmdocker.py
+++ rtmdocker.py
@@ -35,7 +35,7 @@ class Rtmdocker:
 
     def start(self):
         command = self.assume_command(self._args.command)
-        self._command = self.assume_options(self._args, command)
+        self._command = self.assume_options(command)
         # Open x-forwarding
         if self._args.xforward:
             self.enable_x()
@@ -75,6 +75,29 @@ class Rtmdocker:
                        'MyServiceProviderPy : start Python MyServiceProviderComp\n' + \
                        'TkJoyStick          : start Python TkJoyStickComp.py\n' + \
                        'TkLRFViewer         : start Python TkLRFViewer.py\n' + \
+                       'Affine              : start openCV AffineComp\n' + \
+                       'BackGroundSubtractionSimple : start OpenCV BackGroundSubtractionSimpleComp\n' + \
+                       'Binarization        : start OpenCV BinarizationComp\n' + \
+                       'CameraViewer        : start OpenCV CameraViewerComp\n' + \
+                       'Chromakey           : start OpenCV ChromakeyComp\n' + \
+                       'DilationErosion     : start OpenCV DilationErotionComp\n' + \
+                       'Edge                : start OpenCV EdgeComp\n' + \
+                       'Findcontour         : start OpenCV FindcontourComp\n' + \
+                       'Flip                : start OpenCV FlipComp\n' + \
+                       'Histogram           : start OpenCV HistogramComp\n' + \
+                       'Hough               : start OpenCV HoughComp\n' + \
+                       'ImageCalibration    : start OpenCV ImageCalibrationComp\n' + \
+                       'ImageSubstraction   : start OpenCV ImageSubstractionComp\n' + \
+                       'ObjectTracking      : start OpenCV ObjectTrackingComp\n' + \
+                       'OpenCVCamera        : start OpenCV OpenCVCameraComp\n' + \
+                       'Perspective         : start OpenCV PerspectiveComp\n' + \
+                       'RockPaperScissors   : start OpenCV RockPaperScissorsComp\n' + \
+                       'Rotate              : start openCV RotateComp\n' + \
+                       'Scale               : start OpenCV ScaleComp\n' + \
+                       'Sepia               : start openCV SepiaComp\n' + \
+                       'SubstractCaptureImage : start OpenCV SubstractCaptureImageComp\n' + \
+                       'Template            : start openCV TemplateComp\n' + \
+                       'Translate           : start OpenCV TranslateComp\n' + \
                        'bash                : start bash'
         argparser = argparse.ArgumentParser(
             formatter_class=argparse.RawTextHelpFormatter)
@@ -102,29 +125,59 @@ class Rtmdocker:
         return argparser.parse_args()
 
     def assume_command(self, command):
-        example = "/usr/share/openrtm-1.1/example"
-        example_py = "python " + example + "/python"
+        # OpenRTM version check
+        if "120" in self._args.tagname:
+            path_cpp = "/usr/share/openrtm-1.2/components/c++/examples/"
+            path_py = "python /usr/share/openrtm-1.2/components/c++/examples/"
+            path_cv = "/usr/share/openrtm-1.1/components/c++/opencv-rtcs/"
+        else:
+            path_cpp = "/usr/share/openrtm-1.1/example/"
+            path_py = "python " + path_cpp + "/python/"
+            path_cv = "/usr/share/openrtm-1.1/components/c++/opencv-rtcs/"
 
         # command list
         c_dict = {"openrtp": "openrtp",
-                  "Controller": example + "/ControllerComp",
-                  "Motor": example + "/MotorComp",
-                  "ConsoleIn": example + "/ConsoleInComp",
-                  "ConsoleOut": example + "/ConsoleOutComp",
-                  "ConfigSample": example + "/ConfigSampleComp",
-                  "SeqIn": example + "/SeqInComp",
-                  "SeqOut": example + "/SeqOutComp",
-                  "MyServiceConsumer": example + "/MyServiceConsumerComp",
-                  "MyServiceProvider": example + "/MyServiceProviderComp",
-                  "ConsoleInPy": example_py + "/SimpleIO/ConsoleIn.py",
-                  "ConsoleOutPy": example_py + "/SimpleIO/ConsoleOut.py",
-                  "ConfigSamplePy": example_py + "/ConfigSample/ConfigSample.py",
-                  "SeqInPy": example_py + "/SeqIO/SeqIn.py",
-                  "SeqOutPy": example_py + "/SeqIO/SeqOut.py",
-                  "MyServiceConsumerPy": example_py + "/SimpleService/MyServiceConsumer.py",
-                  "MyServiceProviderPy": example_py + "/SimpleService/MyServiceProvider.py",
-                  "TkJoyStick": example_py + "/TkJoyStick/TkJoyStickComp.py",
-                  "TkLRFViewer": example_py + "/TkLRFViewer/TkLRFViewer.py",
+                  "Controller": path_cpp + "ControllerComp",
+                  "Motor": path_cpp + "MotorComp",
+                  "ConsoleIn": path_cpp + "ConsoleInComp",
+                  "ConsoleOut": path_cpp + "ConsoleOutComp",
+                  "ConfigSample": path_cpp + "ConfigSampleComp",
+                  "SeqIn": path_cpp + "SeqInComp",
+                  "SeqOut": path_cpp + "SeqOutComp",
+                  "MyServiceConsumer": path_cpp + "MyServiceConsumerComp",
+                  "MyServiceProvider": path_cpp + "MyServiceProviderComp",
+                  "ConsoleInPy": path_py + "SimpleIO/ConsoleIn.py",
+                  "ConsoleOutPy": path_py + "SimpleIO/ConsoleOut.py",
+                  "ConfigSamplePy": path_py + "ConfigSample/ConfigSample.py",
+                  "SeqInPy": path_py + "SeqIO/SeqIn.py",
+                  "SeqOutPy": path_py + "SeqIO/SeqOut.py",
+                  "MyServiceConsumerPy": path_py + "SimpleService/MyServiceConsumer.py",
+                  "MyServiceProviderPy": path_py + "SimpleService/MyServiceProvider.py",
+                  "TkJoyStick": path_py + "TkJoyStick/TkJoyStickComp.py",
+                  "TkLRFViewer": path_py + "TkLRFViewer/TkLRFViewer.py",
+                  "Affine": path_cv + "AffineComp",
+                  "BackGroundSubtractionSimple": path_cv + "BackGroundSubtractionSimpleComp",
+                  "Binarization": path_cv + "BinarizationComp",
+                  "CameraViewer": path_cv + "CameraViewerComp",
+                  "Chromakey": path_cv + "ChromakeyComp",
+                  "DilationErosion": path_cv + "DilationErotionComp",
+                  "Edge": path_cv + "EdgeComp",
+                  "Findcontour": path_cv + "FindcontourComp",
+                  "Flip": path_cv + "FlipComp",
+                  "Histogram": path_cv + "HistogramComp",
+                  "Hough": path_cv + "HoughComp",
+                  "ImageCalibration": path_cv + "ImageCalibrationComp",
+                  "ImageSubstraction": path_cv + "ImageSubstractionComp",
+                  "ObjectTracking": path_cv + "ObjectTrackingComp",
+                  "OpenCVCamera": path_cv + "OpenCVCameraComp",
+                  "Perspective": path_cv + "PerspectiveComp",
+                  "RockPaperScissors": path_cv + "RockPaperScissorsComp",
+                  "Rotate": path_cv + "RotateComp",
+                  "Scale": path_cv + "ScaleComp",
+                  "Sepia": path_cv + "SepiaComp",
+                  "SubstractCaptureImage": path_cv + "SubstractCaptureImageComp",
+                  "Template": path_cv + "TemplateComp",
+                  "Translate": path_cv + "TranslateComp",
                   "bash": "bash",
                   }
 
@@ -137,7 +190,7 @@ class Rtmdocker:
 
         return c
 
-    def assume_options(self, args, command):
+    def assume_options(self, command):
         # Docker Option
         option_list = []
 
@@ -155,14 +208,14 @@ class Rtmdocker:
         option_list.append(option)
 
         # Device access
-        logging.info("device: " + str(args.device))
-        if args.device:
-            option_device = "--device=" + args.device
+        logging.info("device: " + str(self._args.device))
+        if self._args.device:
+            option_device = "--device=" + self._args.device
             option_list.append(option_device)
 
         # Set X forwarding
-        logging.info("x-forward: " + str(args.xforward))
-        if args.xforward:
+        logging.info("x-forward: " + str(self._args.xforward))
+        if self._args.xforward:
             display = os.environ.get('DISPLAY')
             option_display = "-e DISPLAY=" + display + \
                 " -v /tmp/.X11-unix:/tmp/.X11-unix -v " + \
@@ -170,44 +223,44 @@ class Rtmdocker:
             option_list.append(option_display)
 
         # Add starting xrdp service
-        logging.info("rdp: " + str(args.rdp))
-        if args.rdp:
+        logging.info("rdp: " + str(self._args.rdp))
+        if self._args.rdp:
             command = "/etc/init.d/xrdp start;" + command
 
         # Add starting nameserver
-        logging.info("nameserver: " + str(args.nameserver))
-        if args.nameserver:
+        logging.info("nameserver: " + str(self._args.nameserver))
+        if self._args.nameserver:
             command = "rtm-naming;" + command
 
         # Compile RTC
-        logging.info("compile_component: " + str(args.compile_component))
-        if args.compile_component:
+        logging.info("compile_component: " + str(self._args.compile_component))
+        if self._args.compile_component:
             # Compile C++ component
             command = "apt-get update && apt-get -y install cmake && cd " + \
                 os.getcwd() + " && mkdir -p build && cd build cmake .. && make"
 
         # Startup RTC
-        logging.info("run_component: " + str(args.run_component))
-        if args.run_component:
-            if args.run_component in '.py':
+        logging.info("run_component: " + str(self._args.run_component))
+        if self._args.run_component:
+            if self._args.run_component in '.py':
                 # Python component
-                command = "cd " + os.getcwd() + " && python " + args.run_component
+                command = "cd " + os.getcwd() + " && python " + self._args.run_component
             else:
                 # C++ component
-                command = "cd " + os.getcwd() + " && ./" + args.run_component
+                command = "cd " + os.getcwd() + " && ./" + self._args.run_component
 
         option_network = "--net=host"
         option_list.append(option_network)
-        option_list.append("takahasi/docker-openrtm:" + args.tagname)
+        option_list.append("takahasi/docker-openrtm:" + self._args.tagname)
         option_list.append("\"" + command + "\"")
 
         # Naming randomly
         name = "docker-openrtm-" + str(random.randint(0, 99)) + " "
 
         # Upgrade image
-        logging.info("upgrade: " + str(args.upgrade))
-        if args.upgrade:
-            return "docker pull takahasi/docker-openrtm:" + args.tagname + "&& docker run -ti --rm --name " + name + " ".join(option_list)
+        logging.info("upgrade: " + str(self._args.upgrade))
+        if self._args.upgrade:
+            return "docker pull takahasi/docker-openrtm:" + self._args.tagname + "&& docker run -ti --rm --name " + name + " ".join(option_list)
         else:
             return "docker run -ti --rm --name " + name + " ".join(option_list)
 
